8a782612a0d7d8d77d9afc18fc7edd4ce7da8fda
Dushan Dhananjaya <dushandhananjaya01@gmail.com>
2024-02-15 15:58:11 +0530

    create StackX class

---
+++ src/stack/StackX.java
@@ -0,0 +1,61 @@
+package stack;
+
+public class StackX {
+	private int maxSize;
+	private int top;
+	private double[] stackArray;
+	
+	public StackX(int s) {
+		this.maxSize = s;
+		this.top = -1;
+		this.stackArray = new double[this.maxSize];
+	}
+	
+	public void push(double j) {
+		if(top == maxSize - 1) { // check stackArray is full
+			System.out.println("stack is full");
+		} else {
+			top++;
+			stackArray[top] = j; // stackArray[++top] = j
+		}
+	}
+	
+	public double pop() {
+		if(top == -1) { // check stackArray is empty
+			return -99;
+		} else {
+			double x = stackArray[top]; // double x = stackArray[top--]
+			top--;
+			
+			return x;
+		}
+	}
+	
+	public double peek() {
+		if(top == -1) { // check stackArray is empty
+			return -99;
+		} else {
+			double y = stackArray[top];
+			
+			return y;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return (top == -1);
+	}
+	
+	public boolean isFull() {
+		return (top == (maxSize - 1));
+	}
+	
+	public void print() {
+		if(top == -1) {
+			System.out.println("stack is empty");
+		} else {
+			
+		}
+	}
+	
+	
+}
